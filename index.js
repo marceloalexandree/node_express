b63fb36388b4e49559c7057bbf6151eba35c1eaf
Marcelo <marcelo.afilho@eaportal.org>
2023-10-10 14:12:15 -0300

    recebendo parâmetros na rota

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const path = require("path")
 const app = express()
 const caminhoBase = path.join(__dirname, "templates")
 
+app.get('/usuarios/:id', (req, resp) => {
+    const id = req.params.id
+    console.log(id)
+    resp.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
 app.get('/', (req, resp) => {
     resp.sendFile(`${caminhoBase}/index.html`)
 })
